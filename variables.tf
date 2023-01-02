29c2f2cfd8478d248a397fb644236227a95fec69
BrendaMohan <110720653+BrendaMohan@users.noreply.github.com>
2023-01-02 20:14:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "Main_Server_SG"
+  default     = "Main_Server_SG1"
 }
 
 variable "tag_name" {
